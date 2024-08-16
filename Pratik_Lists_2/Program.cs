222c173c3469ab98ecbc1e52ec181b5fe391955d
Hasan İncel <h_incel@hotmail.com>
2024-08-17 02:07:59 +0300

    Add project files.

---
+++ Pratik_Lists_2/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] kahve = new string[5];
+
+for (int i = 0; i < kahve.Length; i++)
+{
+    Console.WriteLine("Kahve seçiminizi yapın.");
+    string coffe = Console.ReadLine();
+    kahve[i] = coffe;
+}
+
+Console.WriteLine("\n-----Kahve Seçimleriniz-----");
+for (int i = 0;i < kahve.Length;i++)
+{
+    Console.WriteLine(kahve[i]);
+}
